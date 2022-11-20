82bd8688f4fb3d9d99ddff1f199b655ace63bc0b
kujo205 <kuc8301@gmail.com>
2022-11-20 08:57:15 +0200

    20-dark-mode project completed

--- 20-dark-mode/setup/src/App.js
+++ 20-dark-mode/setup/src/App.js
@@ -1,9 +1,52 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect,useRef } from 'react'
 import data from './data'
 import Article from './Article'
 
 function App() {
-  return <h2>dark mode starter</h2>
+  const [theme, setTheme] = useState('light-theme')
+  const mounted=useRef(false);
+
+  const changeThemeHandler=()=>{
+    setTheme(pr=>pr==='light-theme'?'dark-theme':'light-theme');
+  }
+
+  useEffect(()=>{
+    if(!mounted.current){
+      mounted.current=true;
+      return;
+    }
+    document.documentElement.className=theme;
+    if(localStorage.getItem('theme')){
+      localStorage.removeItem('theme');
+      localStorage.setItem('theme',theme);
+    }else{
+      localStorage.setItem('theme',theme);
+    }
+  },[theme]);
+
+  useEffect(()=>{
+    let storageTheme=localStorage.getItem('theme');
+    if(storageTheme){
+      setTheme(storageTheme.replaceAll('\'',''));
+    }
+  },[])
+  
+
+  return <main>
+    <nav>
+    <div className="nav-center">
+      <h1>
+      Overreacted
+      </h1>
+      <button className="btn" onClick={changeThemeHandler}>
+        toggle
+      </button>
+    </div>
+    </nav>
+      <section className="articles">
+        {data.map(el=><Article key={el.id} {...el}/>)}
+      </section>
+  </main>
 }
 
 export default App
--- 20-dark-mode/setup/src/Article.js
+++ 20-dark-mode/setup/src/Article.js
@@ -1,7 +1,20 @@
 import React from 'react'
-import moment from 'moment'
-const Article = () => {
-  return <h2>article component</h2>
+
+const Article = ({title,date, length, snippet})=>{
+
+  return <div className="post">
+  <h2>{title}</h2>
+  <div className="post-info">
+    <span>{date.toLocaleString('en-US',{
+  weekday: 'long',
+  year: 'numeric',
+  month: 'long',
+  day: 'numeric',
+})}</span>
+    <span>{length} min read</span>
+  </div>
+    <p>{snippet}</p>
+</div>
 }
 
 export default Article
